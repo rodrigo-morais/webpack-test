47cd391367d93199cd5bc8db81a426584258ecbd
Rodrigo Morais <morais.rm@gmail.com>
2015-11-19 14:11:35 -0200

    Spliting the code generated by Webpack to load asynchronus.

---
+++ 1.bundle.js
@@ -0,0 +1,20 @@
+webpackJsonp([1],{
+
+/***/ 2:
+/***/ function(module, exports) {
+
+	module.exports = function () {
+	    var calculator, sum;
+
+	    sum = function (a, b) {
+	        return a + b;
+	    };
+
+	    return calculator = {
+	        sum: sum
+	    };
+	};
+
+/***/ }
+
+});
--- app/main.js
+++ app/main.js
@@ -1,15 +1,16 @@
 ﻿'use strict';
 
-var calculator = require('./calculator'),
-    $ = require('../node_modules/jquery/dist/jquery'),
-    number1 = $('#number1'),
-    number2 = $('#number2');
+var $ = require('../node_modules/jquery/dist/jquery');
+
+require(['./calculator.js'], function(calculator) {
+    var number1 = $('#number1'),
+        number2 = $('#number2');
 
 
-$('#sum').on('click', function () {
-    var value1 = parseInt(number1.val()),
-        value2 = parseInt(number2.val());
-
-    alert(calculator().sum(value1, value2));
+    $('#sum').on('click', function () {
+        var value1 = parseInt(number1.val()),
+            value2 = parseInt(number2.val());
+    
+        alert(calculator().sum(value1, value2));
+    })
 });
-
